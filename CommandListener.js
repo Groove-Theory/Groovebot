15008311880123217f8aabf6ab407c2d74043a94
EC2 Default User <ec2-user@ip-172-31-34-98.us-east-2.compute.internal>
2019-04-27 04:40:07 +0000

    test heroku

--- CommandListener.js
+++ CommandListener.js
@@ -8,48 +8,44 @@ const Dictionary = require('./Dictionary.js')
 const Compliment = require('./Compliment.js')
 const Nickname = require('./Nickname.js')
 const RulesCharter = require('./RulesCharter.js')
-exports.Init = function(client)
-{
-  client.on('message', msg => 
-  {
-    if (msg.author.id != client.user.id)
-    {
+exports.Init = function(client) {
+  client.on('message', msg => {
+    if(msg.author.id != client.user.id) {
       var msgChannel = client.channels.get(msg.channel.id);
       var msgText = msg.content;
-      switch(msgText)
-      {
-        case "g!help":
+      switch(msgText) {
+        case "g!help2":
           Help.Init(client, msg);
-          break; 
+          break;
         case "g!vote":
-          Vote.VoteSetup(client, msg); 
-          break; 
+          Vote.VoteSetup(client, msg);
+          break;
         case "g!getquotefile":
-          GrooveQuote.GetFile(client, msg);     
+          GrooveQuote.GetFile(client, msg);
           break;
         case "g!getcode":
-          msg.channel.send(Globals.g_GitLink);  
+          msg.channel.send(Globals.g_GitLink);
           break;
         case "g!stuff":
-          Stuff.Init(client, msg);     
+          Stuff.Init(client, msg);
           break;
         case "g!rules":
           RulesCharter.RulesInit(client, msg);
-          break; 
+          break;
         default:
           if(msgText.substring(0, 13) == "g!quoteupload")
-            GrooveQuote.Upload(client, msg);  
+            GrooveQuote.Upload(client, msg);
           else if(msgText.substring(0, 7) == "g!quote")
-            GrooveQuote.Init(client, msg);  
+            GrooveQuote.Init(client, msg);
           else if(msgText.substring(0, 15) == "g!ventriloquist")
-            Ventriloquist.Change(client, msg); 
+            Ventriloquist.Change(client, msg);
           else if(msgText.substring(0, 12) == "g!compliment")
-            Compliment.Init(client, msg); 
+            Compliment.Init(client, msg);
           else if(msgText.substring(0, 10) == "g!nickname")
-            Nickname.Init(client, msg); 
+            Nickname.Init(client, msg);
           else if(msgText.substring(0, 6) == "t!wiki")
-            Dictionary.Init(client, msg); 
-          else if(msgText.substring(0,2) == "g!")
+            Dictionary.Init(client, msg);
+          else if(msgText.substring(0, 2) == "g!")
             msgChannel.send("The fuck is that shit?");
           break;
       }
@@ -57,5 +53,3 @@ exports.Init = function(client)
   });
 
 }
-
-
