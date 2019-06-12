363e9958ba3ca0aa05b4c72aafa8dec60d9669bf
EC2 Default User <ec2-user@ip-172-31-20-14.us-east-2.compute.internal>
2019-06-12 18:13:44 +0000

    Made keysmash for the memes

---
+++ KeySmash.js
@@ -0,0 +1,32 @@
+const Globals = require('./Globals.js')
+
+exports.Init = function(client, msg)
+{
+  var length = getRandIntFromRange(10, 20)
+  var smash = [];
+
+  while (smash.length < length)
+  {
+    var tierchoose = getRandIntFromRange(32, 126)
+    smash.push(String.fromCharCode(tierchoose))
+  }
+
+  var keysmash = smash.join("")
+
+  var msgChannel = client.channels.get(msg.channel.id);
+  msgChannel.send(keysmash);
+
+}
+
+function getRandItemFromArray(aArray)
+{
+  return aArray[Math.floor(Math.random() * aArray.length)];
+}
+
+function getRandIntFromRange(iLow, iHigh)
+{
+  if (iHigh < iLow)
+    return 0;
+  else
+    return Math.round(((iHigh - iLow) * Math.random()) + iLow)
+}
