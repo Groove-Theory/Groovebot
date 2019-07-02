1344c2bd23810e0a033a15610801a22cee41a583
EC2 Default User <ec2-user@ip-172-31-20-14.us-east-2.compute.internal>
2019-07-02 03:51:29 +0000

    Update FoyerCopy for multiple servers, quick edit to nickname stuff

--- Globals.js
+++ Globals.js
@@ -1,7 +1,8 @@
 exports.bProduction = (process.env.PRODUCTION == 1 || process.env.PRODUCTION == "1");
 ///////////////////TEST VARS////////////////////////////////////////
-exports.g_mainChannelID = "558156350320803851";
-exports.g_copyChannelID = "554769046470656010";
+
+exports.g_mainChannelIDs = ["565734515113328650", "570056315989262346"];
+exports.g_copyChannelIDs = ["571578458124320768", "570056992002015243"];
 
 exports.g_GuildID = "470626956946309144";
 exports.g_SparticistRole = "559075046245793823"
@@ -17,7 +18,9 @@ exports.g_RulesChannel1 = "570056315989262346";
 exports.g_RulesChannel2 = "570056315989262346"
 
 ///////////////////PRODUCTION VARS////////////////////////////////////////
-if(exports.bProduction) {
+if (exports.bProduction)
+{
+  // TODO: We really need to figure out some MongoDB stuff here 
   exports.g_mainChannelID = "526080760101470230";
   exports.g_copyChannelID = "558159040371228683";
 
