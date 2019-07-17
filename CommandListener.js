fde67ccb31ffcfb363ad4f85d04a911c4ca56aeb
Groove Theory <lawar723@att.net>
2019-07-17 21:12:18 +0000

    ChannelListener created to centralize client messages read/less DB calls

--- CommandListener.js
+++ CommandListener.js
@@ -9,53 +9,47 @@ const Nickname = require('./Nickname.js')
 const Idiom = require('./Idiom.js')
 const KeySmash = require('./KeySmash.js')
 const Options = require('./Options.js')
-exports.Init = function(client)
-{
-  client.on('message', msg =>
-  {
-    if (msg.author.id != client.user.id)
-    {
-      var msgChannel = client.channels.get(msg.channel.id);
-      var msgText = msg.content;
-      switch (msgText)
-      {
-        case "g!help":
-          Help.Init(client, msg);
-          break;
-        case "g!vote":
-          Vote.VoteSetup(client, msg);
-          break;
-        case "g!getcode":
-          msg.channel.send(Globals.g_GitLink);
-          break;
-        case "g!idiom":
-          Idiom.Init(client, msg);
-          break;
-        case "g!keysmash":
-          KeySmash.Init(client, msg);
-          break;
-        default:
-          if (msgText.substring(0, 13) == "g!quoteupload")
-            GrooveQuote.Upload(client, msg);
-          else if (msgText.substring(0, 7) == "g!quote")
-            GrooveQuote.Init(client, msg);
-          else if (msgText.substring(0, 15) == "g!ventriloquist")
-            Ventriloquist.Change(client, msg);
-          else if (msgText.substring(0, 12) == "g!compliment")
-            Compliment.Init(client, msg);
-          else if (msgText.substring(0, 10) == "g!nickname")
-            Nickname.Init(client, msg);
-          else if (msgText.substring(0, 9) == "g!options")
-            Options.Init(client, msg);
-          else if (msgText.substring(0, 11) == "g!makequote")
-            GrooveQuote.MakeQuote(client, msg);
-        else if (msgText.substring(0, 6) == "t!wiki")
-            Dictionary.Init(client, msg);
-          else if (msgText.substring(0, 2) == "g!")
-            msgChannel.send("The fuck is that shit?");
-          break;
-      }
-    }
-  });
 
+exports.ProcessMessage = function(client, msg) {
+    if (msg.author.id != client.user.id) {
+        var msgChannel = client.channels.get(msg.channel.id);
+        var msgText = msg.content;
+        switch (msgText) {
+            case "g!help":
+                Help.Init(client, msg);
+                break;
+            case "g!vote":
+                Vote.VoteSetup(client, msg);
+                break;
+            case "g!getcode":
+                msg.channel.send(Globals.g_GitLink);
+                break;
+            case "g!idiom":
+                Idiom.Init(client, msg);
+                break;
+            case "g!keysmash":
+                KeySmash.Init(client, msg);
+                break;
+            default:
+                if (msgText.substring(0, 13) == "g!quoteupload")
+                    GrooveQuote.Upload(client, msg);
+                else if (msgText.substring(0, 7) == "g!quote")
+                    GrooveQuote.Init(client, msg);
+                else if (msgText.substring(0, 15) == "g!ventriloquist")
+                    Ventriloquist.Change(client, msg);
+                else if (msgText.substring(0, 12) == "g!compliment")
+                    Compliment.Init(client, msg);
+                else if (msgText.substring(0, 10) == "g!nickname")
+                    Nickname.Init(client, msg);
+                else if (msgText.substring(0, 9) == "g!options")
+                    Options.Init(client, msg);
+                else if (msgText.substring(0, 11) == "g!makequote")
+                    GrooveQuote.MakeQuote(client, msg);
+                else if (msgText.substring(0, 6) == "t!wiki")
+                    Dictionary.Init(client, msg);
+                else if (msgText.substring(0, 2) == "g!")
+                    msgChannel.send("The fuck is that shit?");
+                break;
+        }
+    }
 }
