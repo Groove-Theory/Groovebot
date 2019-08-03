45964682b6551290f15727fdaa0f19319233dcb2
Groove-Theory <lawar723@att.net>
2019-08-02 19:20:05 -0500

    Updated help commands to current functionality

--- Help.js
+++ Help.js
@@ -1,21 +1,57 @@
-exports.Init = function(client, msg)
-{
+exports.Init = function (client, msg) {
   msg.channel.send(
-  {
-    embed:
     {
-      color: 3447003,
-      title: "Commands List",
-      description: "**g!help**: You just pressed this :laughing: \r\n" +
-        "**g!options**: Setup options for this server\r\n" +
-        "**g!vote**: Trigger anonymous voting (only Councilperson or Code Master can use this)\r\n" +
-        "**g!quote**: Get a random quote \r\n" +
-        "**g!quote <username?> <link?>**: Upload a quote of someone with an option to quickly put in their info or have groovebot help you out (mentions purposefully don't work with this... don't ping them) \r\n" +
-        "**g!compliment <users>**: Send a compliment to yourself or to anyone by mentioning them (mentions only, so DO ping them!)\r\n" +
-        "**g!getcode**: Get a Github Gist of the latest code uploaded for public access \r\n" +
-        "**g!nickname <name>**: Change Groove's nickname with a new nickname of your choosing! \r\n" +
-        "\r\n**\"AskOuija:\"**: Starts a ouija question (only in #ask-ouija). Inputs are only one letter messages. Type \"goodbye\" to end. \r\n" +
-        "**\"Hey Groovebot, \"**: Starts a question for Groovebot (only in #ask-ouija). Inputs are only one word messages (no spaces). Type \"goodbye\" to end. \r\n"
-    }
-  });
+      embed:
+      {
+        color: 3447003,
+        title: "**__Commands List__**",
+        description: "List of Groovebot commands",
+        fields: [
+          {
+            name: "g!help",
+            value: "You just pressed this :laughing: "
+          },
+          {
+            name: "g!options",
+            value: "Setup options for this server (must have 'Manage Server' permissions)"
+          },
+          {
+            name: "g!idiom",
+            value: " Let Groovebot try and come up with a wise saying! "
+          },
+          {
+            name: "g!keysmash",
+            value: " Make Groovebot smash their keyboard! "
+          },
+          {
+            name: "g!quote",
+            value: " Get a random Groove quote "
+          },
+          {
+            name: "g!makequote <message>",
+            value: " Make your own Groove quote "
+          },
+          {
+            name: "g!compliment <users>",
+            value: " Send a compliment to yourself or to anyone by mentioning them "
+          },
+          {
+            name: "g!getcode",
+            value: " Get the Github Repo for Groovebot "
+          },
+          {
+            name: "g!nickname <name>",
+            value: " Change Groove's nickname with a new nickname of your choosing! "
+          },
+          {
+            name: "\"AskOuija:\"",
+            value: " Starts a ouija question (must set up in Options). Inputs are only one letter messages. Type \"goodbye\" to end. "
+          },
+          {
+            name: "\"Hey Groovebot, \"",
+            value: " Starts a question for Groovebot (must set up in Options). Inputs are only one word messages (no spaces). Type \"goodbye\" to end. "
+          },
+        ]
+      }
+    });
 }
