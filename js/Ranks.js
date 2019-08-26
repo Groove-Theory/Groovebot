95e6b9366567e2b1ad2d1b1967c80c52dfa67a8f
Leland Warren <lawar723@att.net>
2019-08-26 14:30:54 -0500

    I think Ranks are done for now

--- js/Ranks.js
+++ js/Ranks.js
@@ -8,142 +8,164 @@ exports.HandleType = {
 
 exports.HandleCategory = function(client, msg, iHandleType)
 {
-    var oMember = msg.member;
-    if(!checkIfMod(oMember))
+    try
     {
-        msg.channel.send("Sorry, you need the 'Manage Server' permission to use this command :sob: ");
-        return;
-    }
-
-    var aMsgContents = msg.content.split(/\s+/);
-    var cCatName = aMsgContents && aMsgContents.length > 1 ? aMsgContents[1] : ""
-
-    if(cCatName == "")
-    {
-        SendReplyMessage(client, msg, "Please enter a category name");
-        return;
-    }
-
-    let oGuild = msg.guild;
-
-    let oKeyObject = {
-        guildID: oGuild.id,
-        production: Globals.Environment.PRODUCTION,
-    }
-
-    let oOptions = {};
-    let cMessage = "Uh oh, there may have been an error..."
-    if(iHandleType == exports.HandleType.ADD)
-    {
-        oOptions = {
-            $addToSet: { categories: {name: cCatName, ranks:[]} }
+        var oMember = msg.member;
+        if(!checkIfMod(oMember))
+        {
+            msg.channel.send("Sorry, you need the 'Manage Server' permission to use this command :sob: ");
+            return;
         }
-        cMessage = "Category **'" + cCatName + "'** successfully added";
-    }
-    else if(iHandleType == exports.HandleType.DELETE)
-    {
-        oOptions = {
-            $pull: { categories: {name: cCatName } }
+
+        var aMsgContents = msg.content.split(/\s+/);
+        var cCatName = aMsgContents && aMsgContents.length > 1 ? aMsgContents[1] : ""
+
+        if(cCatName == "")
+        {
+            SendReplyMessage(client, msg, "Please enter a category name");
+            return;
         }
-        cMessage = "Category **'" + cCatName + "'** has been removed";
-    }
-    else if(iHandleType == exports.HandleType.EDIT)
-    {
-        let cNewCatName = aMsgContents && aMsgContents.length > 1 ? aMsgContents[2] : "";
-        if(cNewCatName)
+
+        let oGuild = msg.guild;
+
+        let oKeyObject = {
+            guildID: oGuild.id,
+            production: Globals.Environment.PRODUCTION,
+        }
+
+        let oOptions = {};
+        let cMessage = "Uh oh, there may have been an error..."
+        if(iHandleType == exports.HandleType.ADD)
         {
             oOptions = {
-                $set: { "categories.name": cNewCatName}
+                $addToSet: { categories: {name: cCatName, ranks:[]} }
             }
-            cMessage = "Category '**" + cCatName + "**' has been renamed to '**" + cNewCatName + "**'";
+            cMessage = "Category **'" + cCatName + "'** successfully added";
         }
+        else if(iHandleType == exports.HandleType.DELETE)
+        {
+            oOptions = {
+                $pull: { categories: {name: cCatName } }
+            }
+            cMessage = "Category **'" + cCatName + "'** has been removed";
+        }
+        else if(iHandleType == exports.HandleType.EDIT)
+        {
+            let cNewCatName = aMsgContents && aMsgContents.length > 1 ? aMsgContents[2] : "";
+            oKeyObject["categories.name"] = cCatName
+            if(cNewCatName)
+            {
+                oOptions = {
+                    $set: { "categories.$.name": cNewCatName}
+                }
+                cMessage = "Category '**" + cCatName + "**' has been renamed to '**" + cNewCatName + "**'";
+            }
+        }
+
+        Globals.Database.UpsertCustom("Ranks", oKeyObject, oOptions, SendReplyMessage(client, msg, cMessage));
     }
-
-    Globals.Database.UpsertCustom("Ranks", oKeyObject, oOptions, SendReplyMessage(client, msg, cMessage));
-
+    catch(err)
+    {
+        SendReplyMessage(client, msg, "Uh oh, there may have been an error...");
+        ErrorHandler.HandleError(client, err)
+    }
 }
 
 exports.HandleCategoryRank = function(client, msg, iHandleType)
 {
-    var oMember = msg.member;
-    if(!checkIfMod(oMember))
+    try
     {
-        msg.channel.send("Sorry, you need the 'Manage Server' permission to use this command :sob: ");
-        return;
-    }
-
-    var aMsgContents = msg.content.split(/\s+/);
-    var cCatName = aMsgContents && aMsgContents.length > 1 ? aMsgContents[1] : ""
-    var cRoleName = aMsgContents && aMsgContents.length > 2 ? aMsgContents[2] : ""
-
-    if(cCatName == "")
-    {
-        SendReplyMessage(client, msg, "Please enter a category name");
-        return;
-    }
-    if(cRoleName == "")
-    {
-        SendReplyMessage(client, msg, "Please enter a role name");
-        return;
-    }
-
-    let oGuild = msg.guild;
-    var oRole = oGuild.roles.find(r => r.name == cRoleName);
-    if(!oRole)
-    {
-        oRole = guild.roles.find(r => r.id == cRoleName);
-        if(!oRole)
+        var oMember = msg.member;
+        if(!checkIfMod(oMember))
         {
-            SendReplyMessage(client, msg, "Sorry, I can't rind that role in this server");
+            msg.channel.send("Sorry, you need the 'Manage Server' permission to use this command :sob: ");
             return;
         }
-    }
 
-    var iRoleID = oRole.id;
-    cRoleName = oRole.name;
+        var aMsgContents = msg.content.split(/\s+/);
+        var cCatName = aMsgContents && aMsgContents.length > 1 ? aMsgContents[1] : ""
+        var cRoleName = aMsgContents && aMsgContents.length > 2 ? aMsgContents.slice(2).join(" ") : ""
 
-    let oKeyObject = {
-        "guildID": oGuild.id,
-        "production": Globals.Environment.PRODUCTION,
-        "categories.name": cCatName
-    }
-
-    let oOptions = {};
-    let cMessage = "Uh oh, there may have been an error..."
-    if(iHandleType == 1)
-    {
-        let cCatIdentifier = "playlists.$[]." + cCatName;
-        oOptions = {
-            $addToSet: { "categories.$.ranks": iRoleID }
+        if(cCatName == "")
+        {
+            SendReplyMessage(client, msg, "Please enter a category name");
+            return;
         }
-        cMessage = `Role **'${oRole.name}'** successfully added to category **'${cCatName}'**`;
-    }
-    else if(iHandleType == 2)
-    {
-        oOptions = {
-            $pull: { "categories.$.ranks": iRoleID }
+        if(cRoleName == "")
+        {
+            SendReplyMessage(client, msg, "Please enter a role name");
+            return;
         }
-        cMessage = `Role **'${oRole.name}'** has been removed from category **'${cCatName}'**`;
+
+        let oGuild = msg.guild;
+        var oRole = oGuild.roles.find(r => r.name == cRoleName);
+        if(!oRole)
+        {
+            oRole = oGuild.roles.find(r => r.id == cRoleName);
+            if(!oRole)
+            {
+                SendReplyMessage(client, msg, `Sorry, I can't find the role ${cRoleName} in this server`);
+                return;
+            }
+        }
+
+        var iRoleID = oRole.id;
+        cRoleName = oRole.name;
+
+        let oKeyObject = {
+            "guildID": oGuild.id,
+            "production": Globals.Environment.PRODUCTION,
+            "categories.name": cCatName
+        }
+
+        let oOptions = {};
+        let cMessage = "Uh oh, there may have been an error..."
+        if(iHandleType == 1)
+        {
+            oOptions = {
+                $addToSet: { "categories.$.ranks": iRoleID }
+            }
+            cMessage = `Role **'${oRole.name}'** successfully added to category **'${cCatName}'**`;
+        }
+        else if(iHandleType == 2)
+        {
+            oOptions = {
+                $pull: { "categories.$.ranks": iRoleID }
+            }
+            cMessage = `Role **'${oRole.name}'** has been removed from category **'${cCatName}'**`;
+        }
+
+
+        Globals.Database.UpsertCustom("Ranks", oKeyObject, oOptions, SendReplyMessage(client, msg, cMessage));
     }
-
-
-    Globals.Database.UpsertCustom("Ranks", oKeyObject, oOptions, SendReplyMessage(client, msg, cMessage));
-
+    catch(err)
+    {
+        SendReplyMessage(client, msg, "Uh oh, there may have been an error...");
+        ErrorHandler.HandleError(client, err)
+    }
 }
 
 exports.ShowCategorysRanks = function(client, msg)
 {
-    var aMsgContents = msg.content.split(/\s+/);
-    var cCatName = aMsgContents && aMsgContents.length > 1 ? aMsgContents[1] : ""
+    try
+    {
+        var aMsgContents = msg.content.split(/\s+/);
+        var cCatName = aMsgContents && aMsgContents.length > 1 ? aMsgContents[1] : ""
 
-    var iMode = cCatName == "" ? 1 : 2; //1 = Categories, 2 = Ranks for a Category
-    if(iMode == 1)
-    {
-        ShowCategories(client, msg)
+        var iMode = cCatName == "" ? 1 : 2; //1 = Categories, 2 = Ranks for a Category
+        if(iMode == 1)
+        {
+            ShowCategories(client, msg)
+        }
+        else if(iMode == 2)
+        {
+            ShowRanks(client, msg, cCatName)
+        }
     }
-    else if(iMode == 2)
+    catch(err)
     {
-        ShowRanks(client, msg, cCatName)
+        SendReplyMessage(client, msg, "Uh oh, there may have been an error...");
+        ErrorHandler.HandleError(client, err)
     }
 }
 
@@ -182,7 +204,6 @@ async function ShowCategories(client, msg)
             cReturn += aResult[i].name + "\r\n";
         }
         cReturn += "```"
-        console.log(cReturn);
         msg.channel.send(cReturn)
     }
 
@@ -218,7 +239,6 @@ async function ShowRanks(client, msg, cCatName)
         var cReturn = "**LIST OF RANKS FOR CATEGORY: " + cCatName.toUpperCase() + "** \r\n```\r\n";
         for(var i = 0; i < aRanks.length; i++)
         {
-            console.log(aRanks[i])
             oRole = oGuild.roles.find(r => r.id == aRanks[i]);
             if(oRole)
             {
@@ -226,7 +246,6 @@ async function ShowRanks(client, msg, cCatName)
             }
         }
         cReturn += "```"
-        console.log(cReturn);
         msg.channel.send(cReturn)
     }
 
@@ -234,95 +253,121 @@ async function ShowRanks(client, msg, cCatName)
 
 exports.PrintRanks = async function(client, msg)
 {
-    let oGuild = msg.guild;
-
-    var aResult = await Globals.Database.dbo.collection("Ranks").aggregate([
-    { $match: {
-        "guildID": oGuild.id,
-        "production": Globals.Environment.PRODUCTION,
-    }},
-    ]).toArray();
-    if (!aResult || aResult.length == 0) {
-        msg.channel.send("Sorry I can't find any ranks or categories for this server")
-        return;
-    }
-    else
+    try
     {
-        var oResult = aResult[0];
-        var oCategories = oResult.categories;
-        var cReturn = "**__LIST OF RANKS AND CATEGORIES__** \r\n\r\n";
-        for(var i = 0; i < oCategories.length; i++)
-        {
-            let oCategory = oCategories[i];
-            cReturn += `**${oCategory.name}**\r\n\`\`\`\r\n` ;
-            for(var j = 0; j < oCategory.ranks.length; j++)
-            {
-                let iRank = oCategory.ranks[j];
-                let oRole = oGuild.roles.find(r => r.id == iRank);
-                let cRoleName = oRole.name;
-                cReturn += `${cRoleName}\r\n`
-            }
-            cReturn += "```";
-        }
-        console.log(cReturn);
-        msg.channel.send(cReturn)
-    }
+        let oGuild = msg.guild;
 
+        var aResult = await Globals.Database.dbo.collection("Ranks").aggregate([
+        { $match: {
+            "guildID": oGuild.id,
+            "production": Globals.Environment.PRODUCTION,
+        }},
+        ]).toArray();
+        if (!aResult || aResult.length == 0) {
+            msg.channel.send("Sorry I can't find any ranks or categories for this server")
+            return;
+        }
+        else
+        {
+            var oResult = aResult[0];
+            var oCategories = oResult.categories;
+            var aReturnString = [];
+            var cReturn = "**__LIST OF RANKS AND CATEGORIES__** \r\n\r\n";
+            for(var i = 0; i < oCategories.length; i++)
+            {
+                let oCategory = oCategories[i];
+                cReturn += `**${oCategory.name}**\r\n\`\`\`\r\n` ;
+                for(var j = 0; j < oCategory.ranks.length; j++)
+                {
+                    let iRank = oCategory.ranks[j];
+                    let oRole = oGuild.roles.find(r => r.id == iRank);
+                    let cRoleName = oRole.name;
+                    cReturn += `${cRoleName}\r\n`
+                }
+                cReturn += "```";
+                if(cReturn.length > 1000)
+                {
+                    aReturnString.push(cReturn)
+                    cReturn = ""
+                }
+            }
+            aReturnString.push(cReturn);
+            for(var i = 0; i < aReturnString.length; i++)
+            {
+                if(aReturnString[i] != "")
+                msg.channel.send(aReturnString[i])
+            }
+            
+        }
+    }
+    catch(err)
+    {
+        SendReplyMessage(client, msg, "Uh oh, there may have been an error...");
+        ErrorHandler.HandleError(client, err)
+    }
 }
 
 
 exports.ToggleUserRank = async function(client, msg)
 {
-    var oMember = msg.member;
-
-    var aMsgContents = msg.content.split(/\s+/);
-    var cRankName = aMsgContents && aMsgContents.length > 1 ? aMsgContents[1] : ""
-
-    if(cRankName == "")
+    try
     {
-        SendReplyMessage(client, msg, "Please enter a rank name");
-        return;
-    }
+        var oMember = msg.member;
 
-    let oGuild = msg.guild;
-    var oRole = oGuild.roles.find(r => r.name == cRankName);
-    if(!oRole)
-    {
-        SendReplyMessage(client, msg, "Sorry, I can't rind that rank");
-        return;
-    }
+        var aMsgContents = msg.content.split(/\s+/);
+        var cRankName = aMsgContents && aMsgContents.length > 1 ? aMsgContents[1] : ""
 
-    var iRoleID = oRole.id;
-    cRankName = oRole.name;
-
-    var aResult = await Globals.Database.dbo.collection("Ranks").find(
+        if(cRankName == "")
         {
-            "guildID": oGuild.id,
-            "production": Globals.Environment.PRODUCTION,
-            "categories.ranks": { $in: [ iRoleID ] }
+            SendReplyMessage(client, msg, "Please enter a rank name");
+            return;
         }
-    ).toArray();
-    let bRankFound = aResult && aResult.length > 0;
-    if(!bRankFound)
-    {
-        SendReplyMessage(client, msg, "Sorry, I can't rind that rank");
-        return;
-    }
-    else
-    {
-        var bHasRoleAlready = oMember.roles.find(r => r.id == iRoleID);
-        let cMessage = "Uh oh, there's been an error..."
-        if(bHasRoleAlready)
+
+        let oGuild = msg.guild;
+        var oRole = oGuild.roles.find(r => r.name == cRankName);
+        if(!oRole)
         {
-            oMember.removeRole(iRoleID)
-            cMessage = "I've removed the role **" + cRankName + "** from you";
+            SendReplyMessage(client, msg, "Sorry, I can't rind that rank");
+            return;
+        }
+
+        var iRoleID = oRole.id;
+        cRankName = oRole.name;
+
+        var aResult = await Globals.Database.dbo.collection("Ranks").find(
+            {
+                "guildID": oGuild.id,
+                "production": Globals.Environment.PRODUCTION,
+                "categories.ranks": { $in: [ iRoleID ] }
+            }
+        ).toArray();
+        let bRankFound = aResult && aResult.length > 0;
+        if(!bRankFound)
+        {
+            SendReplyMessage(client, msg, "Sorry, I can't rind that rank");
+            return;
         }
         else
         {
-            oMember.addRole(iRoleID)
-            cMessage = "I've added the role **" + cRankName + "** for you";
+            var bHasRoleAlready = oMember.roles.find(r => r.id == iRoleID);
+            let cMessage = "Uh oh, there's been an error..."
+            if(bHasRoleAlready)
+            {
+                await oMember.removeRole(iRoleID)
+                cMessage = "I've removed the role **" + cRankName + "** from you";
+            }
+            else
+            {
+                await oMember.addRole(iRoleID)
+                cMessage = "I've added the role **" + cRankName + "** for you";
+            }
+            SendReplyMessage(client, msg, cMessage);
         }
-        SendReplyMessage(client, msg, cMessage);
+    }
+    catch(err)
+    {
+        SendReplyMessage(client, msg, "Sorry, I'm not able to handle that rank...");
+        ErrorHandler.HandleError(client, err)
     }
 }
 
@@ -333,8 +378,4 @@ function checkIfMod(member)
 
 function SendReplyMessage(client, msg, cContent) {
     msg.channel.send(cContent);
-}
-
-function SendReplyMessageInCustomChannel(client, oChannel, cContent) {
-    oChannel.send(cContent);
 }
