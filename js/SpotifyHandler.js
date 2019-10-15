c48b1631ca52eb0db9b5e1852fb4b1437eb68ff8
Leland Warren <lawar723@att.net>
2019-10-15 15:27:33 -0500

    bruh

--- js/SpotifyHandler.js
+++ js/SpotifyHandler.js
@@ -1,7 +1,7 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 const Globals = require('./Globals.js')
 const ErrorHandler = require('./ErrorHandler.js');
-const cron = require('node-schedule');
+//const cron = require('node-schedule');
 
 exports.spotifyApi = {};
 exports.InitSpotifyHandler = function(){
@@ -11,10 +11,10 @@ exports.InitSpotifyHandler = function(){
         clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     });
 
-    refreshToken();
-    var rule = new cron.RecurrenceRule();
-    rule.minute = new cron.Range(0, 59, 55);
-    cron.scheduleJob(rule, refreshToken);
+     refreshToken();
+    // var rule = new cron.RecurrenceRule();
+    // rule.minute = new cron.Range(0, 59, 55);
+    // cron.scheduleJob(rule, refreshToken);
 
 }
 
