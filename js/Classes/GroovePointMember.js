f8bf94d0762ae39b4389fe7dc5b292cd4dbfadd1
Groove Theory <lawar723@att.net>
2019-10-22 05:02:24 +0000

    this was easy

--- js/Classes/GroovePointMember.js
+++ js/Classes/GroovePointMember.js
@@ -1,58 +1,60 @@
+const Globals = require('../Globals.js')
 class GroovePointMember {
   constructor(iGuildID, iUserID) {
     this._iGuildID = iGuildID;
     this._iUserID = iUserID;
   }
-  get iGuildID() {
-    return this._iGuildID;
-  }
-  get iUserID() {
-    return this._iUserID;
-  }
+    get iGuildID() {
+        return this._iGuildID;
+    }
+    get iUserID() {
+        return this._iUserID;
+    }
 
-  get dLastMessage() {
-    return this._dLastMessage;
-  }
-  set dLastMessage(x) {
-    this._dLastMessage = x;
-  }
+    get dLastMessage() {
+        return this._dLastMessage;
+    }
+    set dLastMessage(x) {
+        this._dLastMessage = x;
+    }
 
+    get iPoints() {
+        return this._iPoints;
+    }
 
+    addPoints(iVal)
+    {
+        this._iPoints += iVal;
+    }
 
-
-}
-
-class EmbeddedHelpText {
-  constructor(cCommand, cDesc, cReqArgs, cOpArgs, cExample) {
-    this.cCommand = cCommand;
-    this.cDesc = cDesc;
-    this.cReqArgs = cReqArgs;
-    this.cOpArgs = cOpArgs;
-    this.cExample = cExample;
-    this.oEmbedText = {
-      "color": 3447003,
-      "title": `**__${this.cCommand}__**`,
-      "description": "---------------------------------------",
-      "fields": [
-        {
-          "name": "Description",
-          "value": this.cDesc
-        },
-        {
-          "name": "Required Args",
-          "value": this.cReqArgs ? this.cReqArgs : "(None)"
-        },
-        {
-          "name": "Optional Args",
-          "value": this.cOpArgs ? this.cOpArgs : "(None)"
-        },
-        {
-          "name": "Example",
-          "value": this.cExample
+    async InitUser()
+    {
+        var oQueryObject = {
+            guildID: this._iGuildID,
+            userID: this._iUserID
         }
-      ]
-    };
-  }
+
+        let aResult = await Globals.Database.Query("GroovePoints", oQueryObject);
+        let oResult = aResult.length > 0 ? aResult[0] : null;
+
+        this._iPoints = oResult && oResult["points"] ? oResult["points"] : 0;
+        this._dLastMessage = oResult && oResult["lastawardedmessagedate"] ? oResult["lastawardedmessagedate"] : new Date();
+
+    }
+    UpdateUser()
+    {
+        var oKeyObject = {
+            guildID: this._iGuildID,
+            userID: this._iUserID
+        }
+        var oInsertObject = {
+            "points": this._iPoints,
+            "lastawardedmessagedate": this._dLastMessage
+        };
+
+        Globals.Database.Upsert("GroovePoints", oKeyObject, oInsertObject);
+    }
+
 }
 
-module.exports = EmbeddedHelpText
+module.exports = GroovePointMember
--- js/GroovePoints/GroovePointsMessageHandler.js
+++ js/GroovePoints/GroovePointsMessageHandler.js
@@ -1,22 +1,31 @@
-function processMessage(client, msg)
+const GroovePointMember = require("../Classes/GroovePointMember.js");
+
+async function ProcessMessage(client, msg)
 {
     let oMember = msg.member;
+    let oGuild = msg.guild;
 
+    let oGroovePointMember = new GroovePointMember(oGuild.id, oMember.id);
+    await oGroovePointMember.InitUser();
+    awardPoints(oGroovePointMember, msg.createdAt);
+    oGroovePointMember.UpdateUser();
 }
+exports.ProcessMessage = ProcessMessage;
 
-function awardPoints(oGroovePointMember)
+function awardPoints(oGroovePointMember, dMsgDate)
 {
     let bAward = checkForTimeout(oGroovePointMember.dLastMessage, 1000*60*2);
     if(bAward)
     {
-        oGroovePointMember.iPoints += getRandomValue(1, 10);
+        oGroovePointMember.addPoints(getRandomValue(10, 100));
+        oGroovePointMember.dLastMessage = dMsgDate;
     }
 }
 
 function checkForTimeout(dLastMessage, iTimeoutMilliSec)
 {
     let dNow = new Date()
-    return dNow.getTime() > dLastMessage.getTime + iTimeoutMilliSec;
+    return dNow.getTime() > dLastMessage.getTime() + iTimeoutMilliSec;
 }
 
 function getRandomValue(iMin, iMax)
