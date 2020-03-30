fd6ccc2ec1e6e7180492863b639b8cc0b797e6bb
Warren <lawar723@att.net>
2020-03-29 23:02:53 -0500

    Remove @ in nicknames

--- js/Nickname.js
+++ js/Nickname.js
@@ -16,6 +16,9 @@ exports.Init = async function (client, msg) {
     var cMessage = msg.content;
     var cNewNick = msg.content.substring(11);
 
+    if(StringIsBad(cNewNick) ) // Todo: Get Cheesecord or write findmention string checker
+      throw "bad mention nick"
+
     var oGuild = msg.guild; // client.guilds.get(Globals.g_GuildID);
     var oGrooveUser = oGuild.members.find(m => m.id === Globals.g_GrooveID);
 
@@ -24,6 +27,8 @@ exports.Init = async function (client, msg) {
   }
   catch (err) {
     try{
+      if(StringIsBad(cNewNick) ) // Todo: Get Cheesecord or write findmention string checker
+        throw "bad mention nick"
       var oGrooveBotUser = oGuild.members.find(m => m.id ===client.user.id);
       await oGrooveBotUser.setNickname(cNewNick)
       msg.channel.send("Ok, I changed my name to **" + cNewNick + "**..... I like it!");
@@ -34,4 +39,9 @@ exports.Init = async function (client, msg) {
       ErrorHandler.HandleError(client, err);
     }
   }
+}
+
+function StringIsBad(cString)
+{
+  return cString.indexOf("@") > -1;
 }
