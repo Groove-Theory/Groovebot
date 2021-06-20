41f1704e34e9928cfd09a8d663a0733ffceb9c4c
Groove Theory <lawar723@att.net>
2021-06-20 07:16:39 +0000

    Podcast done

---
+++ js/Podcast.js
@@ -0,0 +1,39 @@
+const Globals = require('./Globals.js')
+const ErrorHandler = require('./ErrorHandler.js');
+const Discord = require('discord.js');
+const EmbeddedHelpText = require("./Classes/EmbeddedHelpText.js");
+
+
+exports.Init = async function (client, msg) {
+
+  iGuildId = "190543951080456192";
+  iPodcastChannelID = "856062925415383060";
+  try {
+    if(msg.channel.guild.id != iGuildId)
+      return;
+
+    let aData = msg.content.split(/\s+/);
+    aData.shift();
+
+    if(!aData || aData.length == 0)
+    {
+        msg.channel.send("You gotta give me a topic.... I mean, shit fam.")
+        return;
+    }
+
+    let cString = aData.join(" ");
+
+    let pChannel = Globals.g_Client.channels.cache.get(iPodcastChannelID);
+    var embed = new Discord.MessageEmbed()
+        .setColor("#426262")
+        .setTitle(`New Podcast Topic!`)
+        .setDescription(cString)
+        .setAuthor(msg.author.tag, msg.author.displayAvatarURL({size:1024, format: "png"}))
+    
+    pChannel.send("", embed)
+
+  }
+  catch (err) {
+    ErrorHandler.HandleError(client, err);
+  }
+}
